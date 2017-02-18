689333d52b7f92e0743d4ccddc8f093d5f8fa0bb
yodalee <lc85301@gmail.com>
2017-02-18 14:53:32 +0800

    revise manifest, observe on delete event

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var gmail;
 
-
 function refresh(f) {
   if( (/in/.test(document.readyState)) || (typeof Gmail === undefined) ) {
     setTimeout('refresh(' + f + ')', 10);
@@ -9,13 +8,17 @@ function refresh(f) {
   }
 }
 
+var playShredSound = function() {
+  console.log("delete something");
+  var myAudio = new Audio();
+}
 
-var main = function(){
+var main = function() {
   // NOTE: Always use the latest version of gmail.js from
   // https://github.com/KartikTalwar/gmail.js
   gmail = new Gmail();
+  gmail.observe.on("delete", playShredSound);
   console.log('Hello,', gmail.get.user_email())
 }
-
 
 refresh(main);
